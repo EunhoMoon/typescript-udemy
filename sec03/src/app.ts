7cdde7ee464059c17627382a0b1d132b6650f0f2
eunho.moon@gmail.com <eunho.moon@gmail.com>
2023-03-28 17:57:12 +0900

    class, this

---
+++ sec03/src/app.ts
@@ -0,0 +1,16 @@
+class Department {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  describe(this: Department) {
+    console.log(`Department: ${this.name}`);
+  }
+}
+
+const accounting = new Department('Accounting');
+
+console.log(accounting);
+accounting.describe();
