80d8af28f42d4c9059205b80af89cc3f05799a9a
eunho.moon@gmail.com <eunho.moon@gmail.com>
2023-03-29 09:33:15 +0900

    class, 접근 제어자

--- sec03/src/app.ts
+++ sec03/src/app.ts
@@ -1,16 +1,30 @@
 class Department {
-  name: string;
+  // private readonly id: string;
+  // private name: string;
+  private employees: string[] = [];
 
-  constructor(name: string) {
-    this.name = name;
+  constructor(private readonly id: string, public name: string) {
+    // this.name = name;
   }
 
   describe(this: Department) {
-    console.log(`Department: ${this.name}`);
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const accounting = new Department('Accounting');
+const accounting = new Department('d1', 'Accounting');
 
-console.log(accounting);
+accounting.addEmployee('Eunho');
+accounting.addEmployee('Max');
+
 accounting.describe();
+accounting.printEmployeeInformation();
