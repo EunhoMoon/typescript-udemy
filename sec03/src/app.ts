676063716afc6a8fad6514b2a765a803096a83dc
eunho.moon@gmail.com <eunho.moon@gmail.com>
2023-03-29 11:59:25 +0900

    static method & static member

--- sec03/src/app.ts
+++ sec03/src/app.ts
@@ -1,10 +1,15 @@
 class Department {
+  static fisicalYear = 2020;
   // private readonly id: string;
   // private name: string;
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.name = name;
+  }
+
+  static createEmployee(name: string) {
+    return { name: name };
   }
 
   describe(this: Department) {
@@ -30,12 +35,37 @@ class ItDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error('No report found.');
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error('Please pass in a valid value!');
+    }
+    this.addReports(value);
+  }
+
   constructor(id: string, private reports: string[]) {
     super(id, 'Accounting');
+    this.lastReport = reports[0];
+  }
+
+  addEmployee(name: string): void {
+    if (name === 'Max') {
+      return;
+    }
+    this.employees.push(name);
   }
 
   addReports(text: string) {
     this.reports.push(text);
+    this.lastReport = text;
   }
 
   getReports() {
@@ -51,10 +81,21 @@ class AccountingDepartment extends Department {
 // accounting.describe();
 // accounting.printEmployeeInformation();
 
+const employee1 = Department.createEmployee('Max');
+console.log(employee1, Department.fisicalYear);
+
 const it = new ItDepartment('i1', ['Eunho']);
 console.log(it);
 
 const accounting = new AccountingDepartment('d2', []);
 
+// console.log(accounting.mostRecentReport);  // error
+accounting.mostRecentReport = 'Year End Report';
+
 accounting.addReports('Somthing went wrong...');
+accounting.addEmployee('Max');
+accounting.addEmployee('Eunho');
 console.log(accounting.getReports());
+accounting.printEmployeeInformation();
+
+console.log(accounting.mostRecentReport);
