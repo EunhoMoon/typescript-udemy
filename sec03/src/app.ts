4dde2a215f70d03c91604bce6dc1651689754848
eunho.moon@gmail.com <eunho.moon@gmail.com>
2023-03-30 11:00:38 +0900

    interface

--- sec03/src/app.ts
+++ sec03/src/app.ts
@@ -1,25 +1,49 @@
-interface Greetable {
-  name: string;
+// type AddFn = (a: number, b: number) => number;
+interface AddFn {
+  (a: number, b: number): number;
+}
 
+let add: AddFn;
+
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
+interface Named {
+  readonly name?: string;
+  outputName?: string;
+}
+
+interface Greetable extends Named {
   greet(phrase: string): void;
 }
 
 class Person implements Greetable {
-  name: string;
-  age: number;
+  name?: string;
+  age = 31;
 
-  constructor(name: string, age: number) {
-    this.name = name;
-    this.age = age;
+  constructor(name?: string) {
+    if (name) {
+      this.name = name;
+    }
   }
 
   greet(phrase: string): void {
-    console.log(`${phrase} ${this.name}, I am ${this.age} old!`);
+    if (this.name) {
+      console.log(`${phrase} ${this.name}, I am ${this.age} old!`);
+    } else {
+      console.log('Hi');
+    }
   }
 }
 
 let user1: Greetable;
 
-user1 = new Person('Eunho', 31);
+user1 = new Person('Eunho');
 
+console.log(user1);
 user1.greet('Hi there - I am');
+
+let user2 = new Person();
+console.log(user2);
+user2.greet('');
