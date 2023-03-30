f5b7b3a9354cb057a1f8e7a18b45c2c24dfd8339
eunho.moon@gmail.com <eunho.moon@gmail.com>
2023-03-30 11:29:18 +0900

    intersection type

--- sec04_advanced_type/src/app.ts
+++ sec04_advanced_type/src/app.ts
@@ -1,1 +1,24 @@
-// Code goes here!
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: 'Eunho',
+  privileges: ['create-server'],
+  startDate: new Date(),
+};
+
+console.log(e1);
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
