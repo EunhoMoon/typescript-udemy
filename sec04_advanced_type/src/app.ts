41ff696b242286cc15b13592c7806338ac7bbaaf
eunho.moon@gmail.com <eunho.moon@gmail.com>
2023-03-31 11:28:52 +0900

    type converting, index, overloading, optional chaining, null 병합

--- sec04_advanced_type/src/app.ts
+++ sec04_advanced_type/src/app.ts
@@ -23,12 +23,32 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
 
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: string, b: number): string;
+function add(a: number, b: string): string;
 function add(a: Combinable, b: Combinable) {
   if (typeof a === 'string' || typeof b === 'string') {
     return a.toString() + b.toString();
   }
   return a + b;
 }
+
+const result = add(1, 5);
+
+const fetchedUserData = {
+  id: 'u1',
+  name: 'Eunho',
+  job: { title: 'CEO', description: 'My own company' },
+};
+
+console.log(fetchedUserData?.job?.title);
+
+const userInput = null;
+
+const storedData = userInput ?? 'DEFAULT';
+
+console.log(storedData);
 
 type UnkonwnEmployee = Employee | Admin;
 
@@ -104,3 +124,19 @@ function moveAnimal(animal: Animal) {
 }
 
 moveAnimal({ type: 'bird', flyingSpeed: 1_000 });
+
+// const userInputElement = <HTMLInputElement>document.getElementById('user-input'!);
+const userInputElement = document.getElementById('user-input');
+
+if (userInputElement) {
+  (userInputElement as HTMLInputElement).value = 'Hi there!';
+}
+
+interface ErrorContainer {
+  [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+  email: 'Not a valid Email',
+  username: 'Must start with a capital character!',
+};
