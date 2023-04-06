a38cdc97b7bece02128d1663e269d42ea6847391
eunho.moon@gmail.com <eunho.moon@gmail.com>
2023-04-06 17:54:34 +0900

    place_app - google map 연동

---
+++ place_app/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.ts',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist'),
+    publicPath: 'dist'
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname),
+    },
+    compress: true,
+
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
+};
