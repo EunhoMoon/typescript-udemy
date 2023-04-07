bdac77885b4c7966edd3583d09fd1cdf4511e94d
eunho.moon@gmail.com <eunho.moon@gmail.com>
2023-04-07 09:27:43 +0900

    place_app - google map 마커

--- place_app/src/app.ts
+++ place_app/src/app.ts
@@ -16,11 +16,16 @@ function searchAddressHandler(event: Event) {
 
   axios.get<GoogleGeocodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`)
       .then(response => {
-        if (response.data.status !== 'OK') {
+        if (response.data.status !== "OK") {
           throw new Error("Could not fetch location");
         }
         const coordinates = response.data.results[0].geometry.location;
-        console.log(coordinates);
+        const map = new google.maps.Map(document.getElementById("map")!, {
+          center: coordinates,
+          zoom: 16
+        });
+
+        new google.maps.Marker({position: coordinates, map: map});
       })
       .catch(error => {
         alert(error.message);
